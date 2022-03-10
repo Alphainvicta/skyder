1fb7276c2567b2a16985f7421b2efbf112240640
Saúl Aceves <acevesaul@gmail.com>
2022-03-10 15:16:13 -0600

    saul v1

--- src/sections/service/service-list.jsx
+++ src/sections/service/service-list.jsx
@@ -14,73 +14,41 @@ class Servicelist extends React.Component{
                 title : 'Levantamiento con Drone',
                 description : 'Supervisa, documenta y obtén un panorama aéreo de tu proyecto',
                 image : iservice1,
-<<<<<<< HEAD
                 details: 'Responde a las necesidades de supervisión de toda clase de obras y proyectos. Somos especialistas en maniobras difíciles y espacios reducidos. El servicio se cobra por hora, media jornada y jornada completa. El material de foto y vídeo es ilimitado durante el tiempo de servicio..',
                 cost: <div>Supervisión de obra<br/>Supervisión de instalación<br/>Vista panorámica<br/>Inspección de terrenos<br/>Fotografía cartográfica<br/>Vuelo en interiores difíciles.</div>,
                 additional: <div>Tenemos experiencia previa ofreciendo cobertura para:<br/>Instalaciones industriales<br/>Fábricas en interior y exterior<br/>Construcciones y desarrollos<br/>Bodegas<br/>Paneles solares<br/>Excavaciones<br/>Terrenos y alrededores</div>,
                 link : '/services/servicio_drone',
                 id : 0
-=======
-                details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-                cost: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-                additional: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-                link : '/services/brand_&_digital',
-                id : 'service-1'
->>>>>>> c41d3159382565af5d00760de1adefa45e77bae6
             },
             {
                 title : 'Contenido para redes sociales',
                 description : 'Plasmamos tu identidad en redes',
                 image : iservice2,
-<<<<<<< HEAD
                 details: 'Somos tus aliados para cubrir las necesidades de comunicación audiovisual de tu empresa. Llevamos un proceso de planeación-ejeución-edición y entrega para crear material que sustentado en una estrategia de comunicación. Contamos con paquetes de producción única o por visita, así como paquetes de producción semanal, mensual y a largo plazo.',
                 cost: <div>Reels para Instagram/Tiktok<br/>Foto y vídeo de producto<br/>Foto y vídeo de catálogo<br/>Foto y vídeo experiencia de servicio<br/>Reviews y tutoriales<br/>Video respuestas a preguntas frecuentes<br/>Making process de tu producto</div>,
                 additional: <div>Sabemos que si no está in internet, no existe(s). Tenemos experiencia diseñando contenido para empresas de productos y de servicios. Si no ves tu giro aquí ¡No te preocupes! Nuestra intención no es hacer anuncios sino historias que conectan con personas; estamos seguros que tu negocio tiene mucho que contar.<br/>Restaurantes y bares<br/>Boutiques<br/>Bazares<br/>Academias<br/>Gimnasios<br/>Hoteles y spas<br/>Casinos.</div>,
                 link : '/services/contenido_redes',
                 id : 1
-=======
-                details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-                cost: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-                additional: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-                link : '/services/graphic_design',
-                id : 'service-2'
->>>>>>> c41d3159382565af5d00760de1adefa45e77bae6
             },
             {
                 title : 'Video para empresas',
                 description : 'Transmitimos el valor de tu mensaje empresarial',
                 image : iservice3,
-<<<<<<< HEAD
                 details: 'Este segmento de vídeo corporativo está pensado para que las empresas puedan compartir mensajes con un tono profesional y de solidez. Las producciones de este tipo incluyen edición avanzada y son tabuladas por jornada o según el proyecto. Se trabaja bajo un proceso de planeación,ejecución y entrega, además de 2 rondas de revisiones. ',
                 cost: <div>Vídeo intitucional<br/>Mensaje directivo<br/>Foto y vídeo conmemorativo<br/>Simulacros<br/>Tour de proceso<br/>Tutoriales y demostraciones<br/>Avances de implementación<br/>Reportes en vídeo<br/>Material de comunicación interna<br/>Video Manual de seguridad</div>,
                 additional: <div>Hemos sido aliados de la comunicación efectiva para empresas del tipo<br/>Sector industrial<br/>Universidades y escuelas<br/>Conferencias y ponencias<br/>Expos</div>,
                 link : '/services/video_empresas',
                 id : 2
-=======
-                details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-                cost: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-                additional: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-                link : '/services/advertising',
-                id : 'service-3'
->>>>>>> c41d3159382565af5d00760de1adefa45e77bae6
             },
             {
                 title : 'Cobertura de eventos',
                 description : 'Capturamos la mejor experiencia de tu evento.',
                 image : iservice4,
-<<<<<<< HEAD
                 details: 'Sabemos capturar los momentos claves. Sin excepción. inmortaliza tu evente y dale acceso a tu públilco a un resumen dinámico y con una narrativa precisa de lo que pasó en tu evento. ¡No querrán perdérselo de nuevo! Nuestro equipo está preparado con foto y vídeo para todo tipo de ocasiones. Los tabuladores son por hora, media jornada y jornada completa e incuyen planeación de los momentos claves y edición avanzada. Aparta tu fecha y deja tu evento en manos de los expertos.',
                 cost: <div>Conciertos y live sessions<br/>Inauguraciones y clausuras<br/>Presentaciones de producto<br/>Cocktails<br/>Sex reveal y sociales<br/>Ferias y Bazares<br/>Eventos escolares<br/>Eventos empresariales<br/>Partidos y maratones<br/>Torneos y dinámicas<br/>Premiaciones y ceremonias<br/>Marchas y desfiles<br/>Pirotecnia y shows</div>,
                 additional: <div>No tenemos límites cuando se trata del tipo de evento a cubrir, pero algunos ejemplos de empresas satisfechas son:<br/>Estadios<br/>Escuelas<br/>Ligas deportivas<br/>Salones de fiestas<br/>Hoteles y Expos<br/>Planners<br/>Agencias de marketing<br/>Centros comerciales<br/>Espacios públicos</div>,
                 link : '/services/cobertura_eventos',
                 id : 3
-=======
-                details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-                cost: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-                additional: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-                link : '/services/visual_identity',
-                id : 'service-4'
->>>>>>> c41d3159382565af5d00760de1adefa45e77bae6
             }]
         }
     }
