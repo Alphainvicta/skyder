558117e425c46762ba33afda3b87e706859b531b
Paul <alphaceves@gmail.com>
2022-03-10 19:10:10 -0600

    numero telefono

--- src/sections/footer/footer.jsx
+++ src/sections/footer/footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
         <div className='rights'>
             Skyder © 2022 - todos los derechos reservados.
         </div>
-        <a className='number' href="https://wa.me/523317516117" target='_blank' rel='noreferrer'>(33) 1751-6117</a>
+        <a className='number' href="https://wa.me/523313058825" target='_blank' rel='noreferrer'>(33) 1305-8825</a>
         <a className='mail' href="mailto:ventas@skyder.mx" target='_blank' rel='noreferrer'>ventas@skyder.mx</a>
     </div>
 );
