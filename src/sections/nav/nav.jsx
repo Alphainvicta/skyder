a8c4181c3b7aafdf9cc2bc9f59287a14b0e5de08
Paul <alphaceves@gmail.com>
2022-05-17 21:44:48 -0500

    modified to navigator v2

--- src/sections/nav/nav.jsx
+++ src/sections/nav/nav.jsx
@@ -12,35 +12,45 @@ const Nav = () => {
     const [visible, setVisible] = useState(false);
     
     return(
-    <div className='nav-cont'>
-        <div className='logo-icon'>
-            <Link to="/"> <Logo/> </Link>
-        </div>
-        <a className='instagram' href='https://www.instagram.com/skydergdl/' target='_blank' rel='noreferrer'><Instagram/></a>
-        <a className='behance' href='https://www.behance.net/skyder' target='_blank' rel='noreferrer'><Behance/></a>
-        <div className='burguer'>
-            <button onClick={() => setVisible(true)}><Burguer/></button>
+
+    <div className='nav'>
+        <div className='nav-cont'>
+            <div className='logo-icon'>
+                <Link to="/"> <Logo/> </Link>
+            </div>
+            <a className='instagram' href='https://www.instagram.com/skydergdl/' target='_blank' rel='noreferrer'><Instagram/></a>
+            <a className='behance' href='https://www.behance.net/skyder' target='_blank' rel='noreferrer'><Behance/></a>
+            <div className='burguer'>
+                <button onClick={() => setVisible(true)}><Burguer/></button>
+            </div>
         </div>
         {visible && 
-        <div className='burgernav-cont'>
-            <div className='Home-link'>
-                <Link to="/"> Home </Link>
+            <div className='burgernav-cont'>
+                
+                <div className='top-cont'>
+                    <div className='logo-icon'>
+                    <Link to="/" onClick={() => setVisible(false)}> <Logo/> </Link>
+                    </div>
+                    <a className='instagram' href='https://www.instagram.com/skydergdl/' target='_blank' rel='noreferrer'><Instagram/></a>
+                    <a className='behance' href='https://www.behance.net/skyder' target='_blank' rel='noreferrer'><Behance/></a>
+                    <div className='bclose'>
+                        <button onClick={() => setVisible(false)}><Bclose/></button>
+                    </div>
+                </div>
+
+                <div className='bottom-cont'>
+                    <Link to="/" onClick={() => setVisible(false)}> inicio </Link>
+                    <p>servicios</p>
+                    <Link className='services-links' to="/services/servicio_drone" onClick={() => setVisible(false)}> Levantamiento Drone </Link>
+                    <Link className='services-links' to="/services/contenido_redes" onClick={() => setVisible(false)}> Contenido para redes </Link>
+                    <Link className='services-links' to="/services/video_empresas" onClick={() => setVisible(false)}> Vídeo para empresas </Link>
+                    <Link className='services-links' to="/services/cobertura_eventos" onClick={() => setVisible(false)}> Cobertura de eventos </Link>
+                    <Link to="/quote" onClick={() => setVisible(false)}> Contacto </Link>
+                </div>
             </div>
-            <div className='services-links'>
-                <Link to="/services/servicio_drone"> Levantamiento Drone </Link>
-                <Link to="/services/contenido_redes"> Contenido para redes </Link>
-                <Link to="/services/video_empresas"> Vídeo para empresas </Link>
-                <Link to="/services/cobertura_eventos"> Cobertura de eventos </Link>
-            </div>
-            <div className='quote-link'>
-                <Link to="/quote"> Contacto </Link>
-            </div>
-            <div className='burguer'>
-                <button onClick={() => setVisible(false)}><Bclose/></button>
-            </div>
-        </div>
         }
-    </div>)
+    </div>
+    )
 }
 
 export default Nav;
