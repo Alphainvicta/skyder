4e766200a616c7752e563ff7917cfc7bd5850d8f
unknown <alphaceves@gmail.com>
2024-11-05 18:19:35 -0600

    created react app, testing nested css

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="simongrid">
+        <div className="gg x1">1</div>
+        <div className="gg x2">2</div>
+        <div className="gg x3">3</div>
+        <div className="gg x4">4</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
