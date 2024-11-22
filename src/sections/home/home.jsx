adc801c4899ee6f19a8a898781a1da1b02f6c40a
unknown <alphaceves@gmail.com>
2024-11-21 23:19:17 -0600

    fixed home vector

--- src/sections/home/home.jsx
+++ src/sections/home/home.jsx
@@ -2,18 +2,12 @@ import React from "react";
 import "./home.css";
 import Homelist from "../../components/home-service/home-service-list.jsx";
 
-import { ReactComponent as Lvector } from "../../icons/left-vector.svg";
-import { ReactComponent as Rvector } from "../../icons/right-vector.svg";
+import { ReactComponent as Homevector } from "../../icons/homeVector.svg";
 
 const Home = () => (
   <div className="home_cont">
     <div className="vector_cont">
-      <div className="lvector">
-        <Lvector viewBox="0 0 277 650" />
-      </div>
-      <div className="rvector">
-        <Rvector viewBox="0 0 376 808" />
-      </div>
+      <Homevector />
     </div>
     <div className="main_screen">
       <div className="main_title">
